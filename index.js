c4d13e7c9a0037e5038a7a5767b4b9ac67ca4522
Beaglefoot <ch-e-st@yandex.ru>
2018-04-22 21:14:41 +0300

    Handle POST to /shorten path

--- index.js
+++ index.js
@@ -9,16 +9,36 @@ const currentIp = getCurrentIp();
 const currentTime = getCurrentTime();
 
 const app = express();
+const shortenPath = '/shorten';
 
 app.use((req, _, next) => {
   console.log(`\x1b[33m--- ${req.method} ---\x1b[0m`);
   next();
 });
 
+app.use((req, res, next) => {
+  if (req.path === shortenPath) {
+    res.append('Allow', 'POST, OPTIONS');
+
+    if (req.method === 'OPTIONS') {
+      res.send();
+      return;
+    }
+
+    if (req.method !== 'POST') {
+      res.sendStatus(405);
+      return;
+    }
+  }
+
+  next();
+});
+
 app.use(express.static('public'));
 
-app.get('*', (req, res) => {
-  res.sendStatus(404);
+app.post(shortenPath, (req, res) => {
+  console.log('shorten');
+  res.send();
 });
 
 app.listen(PORT, () =>
